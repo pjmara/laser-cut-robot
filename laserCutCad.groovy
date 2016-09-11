9475ec1f65d34c13e2ba7bb5bfbe36ed8cda5b96
madhephaestus <mad.hephaestus@gmail.com>
2016-09-11 17:03:35 -0400

    starting the laser cut cad process
    Adding the servos in thier kinematic spaces

--- laserCutCad.groovy
+++ laserCutCad.groovy
@@ -2,26 +2,116 @@ import com.neuronrobotics.bowlerstudio.creature.ICadGenerator;
 import com.neuronrobotics.bowlerstudio.creature.CreatureLab;
 import org.apache.commons.io.IOUtils;
 import com.neuronrobotics.bowlerstudio.vitamins.*;
-//First we load teh default cad generator script 
-ICadGenerator defaultCadGen=(ICadGenerator) ScriptingEngine
-	                    .gitScriptRun(
-                                "https://gist.github.com/bcb4760a449190206170.git", // git location of the library
-	                              "ThreeDPrintCad.groovy" , // file to load
-	                              null
-                        )
+
 return new ICadGenerator(){
+	HashMap<String , HashMap<String,ArrayList<CSG>>> map =  new HashMap<>();
+	HashMap<String,ArrayList<CSG>> bodyMap =  new HashMap<>();
+
+	/**
+	 * Gets the all dh chains.
+	 *
+	 * @return the all dh chains
+	 */
+	public ArrayList<DHParameterKinematics> getLimbDHChains(MobileBase base) {
+		ArrayList<DHParameterKinematics> copy = new ArrayList<DHParameterKinematics>();
+		for(DHParameterKinematics l:base.getLegs()){
+			copy.add(l);	
+		}
+		for(DHParameterKinematics l:base.getAppendages() ){
+			copy.add(l);	
+		}
+		return copy;
+	}
+	
 	@Override 
-	public ArrayList<CSG> generateCad(DHParameterKinematics d, int linkIndex) {
-		ArrayList<CSG> allCad=defaultCadGen.generateCad(d,linkIndex);
-		//If you want you can add things here
-		//allCad.add(myCSG);
-		return allCad;
+	public ArrayList<CSG> generateBody(MobileBase base ){
+		
+		String legStr =""
+		for(DHParameterKinematics l:getLimbDHChains(base)){
+			legStr+=l.getRobotToFiducialTransform(). getXml();
+		}
+		if(bodyMap.get(legStr)!=null){
+			println "Body cached"
+			for(CSG csg:bodyMap.get(legStr))
+				csg.setManipulator(base.getRootListener());
+			return bodyMap.get(legStr)
+		}
+		ArrayList<CSG> cutouts=new ArrayList<>();
+		ArrayList<CSG> attach=new ArrayList<>();
+		
+		def bodyParts = []as ArrayList<CSG>
+		bodyMap.put(legStr,bodyParts)
+		return bodyParts;
 	}
 	@Override 
-	public ArrayList<CSG> generateBody(MobileBase b ) {
-		ArrayList<CSG> allCad=defaultCadGen.generateBody(b);
-		//If you want you can add things here
-		//allCad.add(myCSG);
-		return allCad;
+	public ArrayList<CSG> generateCad(DHParameterKinematics sourceLimb, int linkIndex) {
+		String legStr = sourceLimb.getXml()
+		LinkConfiguration conf = sourceLimb.getLinkConfiguration(linkIndex);
+
+		String linkStr =conf.getXml()
+		ArrayList<CSG> csg = null;
+		HashMap<String,ArrayList<CSG>> legmap=null;
+		if(map.get(legStr)==null){
+			map.put(legStr, new HashMap<String,ArrayList<CSG>>())	
+			// now load the cad and return it. 
+		}
+		legmap=map.get(legStr)
+		if(legmap.get(linkStr) == null ){
+			legmap.put(linkStr,new ArrayList<CSG>())
+		}
+		csg = legmap.get(linkStr)
+		if(csg.size()>linkIndex){
+			// this link is cached
+			println "This link is cached"
+			return csg;
+		}
+		//Creating the horn
+		ArrayList<DHLink> dhLinks=sourceLimb.getChain().getLinks();
+		DHLink dh = dhLinks.get(linkIndex);
+		HashMap<String, Object> shaftmap = Vitamins.getConfiguration(conf.getShaftType(),conf.getShaftSize())
+		double hornOffset = 	shaftmap.get("hornThickness")	
+		
+		// creating the servo
+		CSG servoReference=   Vitamins.get(conf.getElectroMechanicalType(),conf.getElectroMechanicalSize())
+		.transformed(new Transform().rotZ(-90))
+		
+		double servoTop = servoReference.getMaxZ()
+		CSG horn = Vitamins.get(conf.getShaftType(),conf.getShaftSize())	
+					.movez(servoTop)
+
+		
+		
+		if(linkIndex==0){
+			add(csg,servoReference,sourceLimb.getRootListener())
+			add(csg,horn,sourceLimb.getRootListener())
+		}else{
+			add(csg,moveDHValues(servoReference,dh),dh.getListener())
+			add(	csg,horn,dh.getListener())
+		}
+		
+		return csg;
+	}
+
+	private CSG reverseDHValues(CSG incoming,DHLink dh ){
+		println "Reversing "+dh
+		return incoming
+			.rotx(Math.toDegrees(-dh.getAlpha()))
+			.movex(dh.getR()/2)		
+	}
+	
+	private CSG moveDHValues(CSG incoming,DHLink dh ){
+		return incoming.transformed(new Transform().translateZ(-dh.getD()))
+		.transformed(new Transform().rotZ(-Math.toDegrees(dh.getTheta())))
+		.transformed(new Transform().rotZ((90+Math.toDegrees(dh.getTheta()))))
+		.transformed(new Transform().translateX(-dh.getR()))
+		.transformed(new Transform().rotX(Math.toDegrees(dh.getAlpha())));
+		
+	}
+
+
+	private add(ArrayList<CSG> csg ,CSG object, Affine dh ){
+		object.setManipulator(dh);
+		csg.add(object);
+		BowlerStudioController.addCsg(object);
 	}
 };
