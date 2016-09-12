ea45e1d47828aeb9819c162d1cf8ed2899eca76a
madhephaestus <mad.hephaestus@gmail.com>
2016-09-11 22:01:20 -0400

    Body loading head

--- neckCad.groovy
+++ neckCad.groovy
@@ -16,6 +16,8 @@ return new ICadGenerator(){
 	LengthParameter headDiameter 		= new LengthParameter("Head Dimeter",100,[200,50])
 	LengthParameter snoutLen 		= new LengthParameter("Snout Length",63,[200,50])
 	LengthParameter jawHeight 		= new LengthParameter("Jaw Height",32,[200,10])
+	LengthParameter leyeDiam 		= new LengthParameter("Left Eye Diameter",35,[headDiameter.getMM()/2,29])
+	LengthParameter reyeDiam 		= new LengthParameter("Right Eye Diameter",35,[headDiameter.getMM()/2,29])
 	LengthParameter eyeCenter 		= new LengthParameter("Eye Center Distance",headDiameter.getMM()/2,[headDiameter.getMM(),headDiameter.getMM()/2])
 	StringParameter servoSizeParam 			= new StringParameter("hobbyServo Default","towerProMG91",Vitamins.listVitaminSizes("hobbyServo"))
 	StringParameter boltSizeParam 			= new StringParameter("Bolt Size","M3",Vitamins.listVitaminSizes("capScrew"))
@@ -38,6 +40,8 @@ return new ICadGenerator(){
 			headDiameter.setMM(180)
 			snoutLen.setMM(150)
 			eyeCenter.setMM(100)
+			leyeDiam.setMM(60)
+			reyeDiam.setMM(60)
 			ArrayList<CSG> headParts = (ArrayList<CSG> )ScriptingEngine.gitScriptRun("https://gist.github.com/e67b5f75f23c134af5d5054106e3ec40.git", "AnimatronicHead.groovy" ,  null )
 			for(int i=0;i<headParts.size()-1;i++){
 				CSG part = headParts.get(i)
