7dffe13b6167bed5ace17fe805fe6987e169d6a1
Pj Mara <funpjm@gmail.com>
2016-11-03 14:13:14 -0400

    Added servo and horn creation

--- feetCad.groovy
+++ feetCad.groovy
@@ -38,6 +38,16 @@ class Feet implements ICadGenerator, IParameterChanged{
 		ArrayList<CSG> allCad=defaultCadGen.generateCad(d,linkIndex);
 		ArrayList<DHLink> dhLinks=d.getChain().getLinks();
 		DHLink dh = dhLinks.get(linkIndex)
+		LinkConfiguration conf = sourceLimb.getLinkConfiguration(linkIndex);
+		HashMap<String, Object> shaftmap = Vitamins.getConfiguration(conf.getShaftType(),conf.getShaftSize())
+		double hornOffset = 	shaftmap.get("hornThickness")	
+		
+		// creating the servo
+		CSG servoReference=   Vitamins.get(conf.getElectroMechanicalType(),conf.getElectroMechanicalSize())
+		.transformed(new Transform().rotZ(90))
+		
+		double servoTop = servoReference.getMaxZ()
+		CSG horn = Vitamins.get(conf.getShaftType(),conf.getShaftSize())	
 		
 		//If you want you can add things here
 		//allCad.add(myCSG);
